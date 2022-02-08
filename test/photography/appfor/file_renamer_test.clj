4f0a5a831619b23576cea8a20c91519d5ec3d447
Paul Williams <paul@paulswilliams.me.uk>
2022-02-08 06:16:39 +0000

    Initial project creation
    clojure -X:new :template lib :name photography.appfor/file-renamer

---
+++ test/photography/appfor/file_renamer_test.clj
@@ -0,0 +1,7 @@
+(ns photography.appfor.file-renamer-test
+  (:require [clojure.test :refer :all]
+            [photography.appfor.file-renamer :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
