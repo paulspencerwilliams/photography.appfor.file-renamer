486b375158556daf7590814d242d688a6e41db5d
Paul Williams <paul@paulswilliams.me.uk>
2022-02-13 09:53:56 +0000

    Return name and date metadata for all raw files

--- src/photography/appfor/io.clj
+++ src/photography/appfor/io.clj
@@ -1,8 +1,17 @@
 (ns photography.appfor.io
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:import [com.drew.imaging ImageMetadataReader]
+           [com.drew.metadata.exif ExifSubIFDDirectory]))
 
-(defn get-raw-files [dir]
-  (->> dir
-      io/file
-      .listFiles
-      (filter #(not (.isDirectory %)))))
+(defn raw-files-metadata [dir]
+    (->> dir
+        io/file
+        .listFiles
+        (filter #(not (.isDirectory %)))
+        (map #(let [metadata
+                   (-> % 
+                       ImageMetadataReader/readMetadata
+                       (.getFirstDirectoryOfType ExifSubIFDDirectory))]
+               {:filename             (.getName %)
+                :original-time        (.getDate metadata ExifSubIFDDirectory/TAG_DATETIME_ORIGINAL)
+                :original-time-offset (.getDate metadata ExifSubIFDDirectory/TAG_TIME_ZONE_ORIGINAL)}))))
--- test/photography/appfor/io_test.clj
+++ test/photography/appfor/io_test.clj
@@ -1,15 +1,15 @@
 (ns photography.appfor.io-test
   (:require [clojure.test :refer :all]
             [photography.appfor.io :as sut]
-            [clojure.java.io :as io]))
+            [clojure.java.io :refer [resource]]))
 
 (deftest get-raw-files 
   (testing "Check count and name"
     (let [raw-files (->> "raw-folder"
-                         io/resource
-                         sut/get-raw-files) ]
-      (is (= [(->> "raw-folder/raw-file.JPG"
-                   io/resource
-                   .toURI
-                   java.io.File.)]
+                         resource
+                         sut/raw-files-metadata) ]
+      (is (= [{:filename "raw-file.JPG"
+               :original-time #inst "2006-03-02T16:56:22.000-00:00"
+               :original-time-offset nil}]
              raw-files)))))
+
