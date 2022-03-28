f415982d940a6c6d694fa1c51721c049da6681f1
Paul Williams <paul@paulswilliams.me.uk>
2022-03-28 19:33:18 +0100

    Basic ingester
    Backsup, and attempts to copy ingest files to geotagged/yes directory

--- src/photography/appfor/ingester.clj
+++ src/photography/appfor/ingester.clj
@@ -1,6 +1,14 @@
-(ns photography.appfor.ingester)
+(ns photography.appfor.ingester
+  (:require [photography.appfor.io :as io]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
-  (prn x "Hello, World!"))
+  (let [to-ingest (io/raw-files-to-ingest)]
+    (do (io/ensure-back-up-dir)
+        (io/ensure-possibly-geotagged-dirs)
+        (doall (map #(do (io/back-up-file %)
+                         (println (str "Backing up file                              " %))
+                         (io/move-to-geotagged-yes-dir %)
+                         (println (str "Move ASSUMED geotagged file to geotagged/yes " %))) to-ingest))
+        (io/delete-back-up-dir))))
--- src/photography/appfor/io.clj
+++ src/photography/appfor/io.clj
@@ -4,6 +4,14 @@
   (:import [com.drew.imaging ImageMetadataReader]
            [com.drew.metadata.exif ExifSubIFDDirectory]))
 
+(defn expand-home [s] (clojure.string/replace-first s "~" (System/getProperty "user.home")))
+
+(def ingest-path (expand-home "~/Pictures/photography/1-raw-ingest"))
+
+(def back-up-path (str ingest-path "/backup"))
+
+(def geotagged-yes-path (expand-home "~/Pictures/photography/2-possibly-geotagged/yes"))
+
 (defn date-function [d] [(.getDate d ExifSubIFDDirectory/TAG_DATETIME_ORIGINAL)
                          (.getDate d ExifSubIFDDirectory/TAG_DATETIME_DIGITIZED)])
 
@@ -33,3 +41,37 @@
        (filter #(not (.isDirectory %)))
        (map raw-file-metadata)
        (sort-by :filename)))
+
+(defn raw-files-to-ingest []
+  (->> (io/file ingest-path)
+       .listFiles
+       (filter #(not (or (.isDirectory %)
+                         (= (.getName %) ".DS_Store"))))))
+
+(defn ensure-back-up-dir []
+  (if (not (.mkdir (io/file back-up-path)))
+    (throw (Exception. (str "Could not create back up dir: " back-up-path)))))
+
+(defn back-up-file [f]
+  (io/copy f (io/file (str back-up-path "/" (.getName f)))))
+
+(defn move-to-geotagged-yes-dir [f]
+  (let [file-name      (-> f
+                           raw-file-metadata
+                           :original-utc-time
+                           (clojure.string/replace #":" "-"))
+        file-extension (last (re-matches #".*\.(.*)" (.getName f)))
+        dest-path      (str geotagged-yes-path  "/" file-name "." file-extension)]
+    (if (not (.renameTo f (io/file dest-path)))
+      (throw (Exception. (str "Could not move file to : " dest-path))))))
+
+(defn ensure-possibly-geotagged-dirs []
+  (do (if (not (.mkdir (io/file geotagged-yes-path)))
+        (throw (Exception. (str "Could not create geotagged yes path: " geotagged-yes-path)))) ))
+
+(defn delete-back-up-dir []
+  (letfn [(delete-dir [f]
+            (when (.isDirectory f)
+              (run! delete-dir (.listFiles f)))
+            (io/delete-file f))]
+    (delete-dir (io/file back-up-path))))
